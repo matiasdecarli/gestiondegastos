99e986a88c2ed8cc9c99f2d7c50035de28816eef
Matias De Carli <matiasdecarli@gmail.com>
2018-01-07 18:56:56 -0300

    show more movements

--- list_movements.php
+++ list_movements.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    if (!isset ($_SESSION["user"])){  
-      header('Location: enter.php');  
+    if (!isset ($_SESSION["user"])){
+      header('Location: enter.php');
     }
     include('header.html');
 ?>
@@ -10,30 +10,30 @@
      <?php include('menu.html'); ?>
       <hr>
       <div class="jumbotron">
-        <h1>Listado de Ultimos Movimientos</h1>        
-      </div>    
-      <?php 
+        <h1>Listado de Ultimos Movimientos</h1>
+      </div>
+      <?php
         include("functions.php");
-        $movements = getLastMovements(20);        
-      ?>      
+        $movements = getLastMovements(70);
+      ?>
         <table class="table table-striped">
             <thead>
               <tr>
                 <th>Fecha</th>
                 <th>Importe</th>
-                <th>Concepto</th>              
+                <th>Concepto</th>
                 <!-- <th>Saldo</th> -->
                 <th>Autor</th>
                 <th>Detalle</th>
               <tr>
             </thead>
             <tbody>
-              <?php                 
-                foreach ($movements as $movement){ 
+              <?php
+                foreach ($movements as $movement){
                     if ($movement['tipo']=='Ingreso')
                         echo '<tr class="success">';
-                    else                           
-                      echo '<tr class="error">';            
+                    else
+                      echo '<tr class="error">';
                       $date = new DateTime($movement['fecha']);
                       echo '<td>'.$date->format('d/m/Y');'</td>';
                       echo '<td>'.number_format($movement['importe'],2,',','.') .'</td>';
@@ -42,11 +42,11 @@
                       echo '<td><a href="detail.php?id='.$movement['id'].'" class=" icon-question-sign"></a></td>';
                     echo '<tr>';
                 } ?>
-            </tbody>          
+            </tbody>
           </table>
         <hr>
-    </div> 
+    </div>
   </body>
-<?php 
+<?php
   include('footer.html');
  ?>
